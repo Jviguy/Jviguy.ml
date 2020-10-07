58ad7ee5a9cf8bc3d82634686b02d19fbc55439a
jviguy <jviguytwo2@gmail.com>
2020-10-06 21:53:15 -0400

    Put in the build so that i can just git clone on my vm

---
+++ build/precache-manifest.9f1003ccdcfc67bcd7e10f86b45ec914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c8eecbb9354f27e468155cdffa311e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f9fa66df41eae60aff4",
+    "url": "/static/css/main.53b3799e.chunk.css"
+  },
+  {
+    "revision": "6b2ee25761ebae145d38",
+    "url": "/static/js/2.98c8eefc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98c8eefc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9fa66df41eae60aff4",
+    "url": "/static/js/main.44dbe808.chunk.js"
+  },
+  {
+    "revision": "5ad83cd8f3b630dafa55",
+    "url": "/static/js/runtime-main.30caf87d.js"
+  }
+]);
